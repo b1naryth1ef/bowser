a3bbf396d942ad85c82b0fad3b7894874b2f32b1
Andrei <b1naryth1ef@gmail.com>
2017-01-09 15:17:58 -0800

    Add packaging scripts

---
+++ packaging/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -z $VERSION ]; then
+  echo "Please set the VERSION env variable"
+  exit 1
+fi
+
+mkdir -p build
+pushd build
+
+# Create build folders for package
+mkdir -p usr/bin
+mkdir -p etc
+
+# Build bowse
+go build ../../cmd/bowser/bowser.go
+go build ../../cmd/bowser-create-account/bowser-create-account.go
+
+# Copy files in place
+mv bowser usr/bin/
+mv bowser-create-account usr/bin/
+cp -r bowser etc/
+
+popd
+
+fpm \
+  -s dir \
+  -t deb \
+  -v $VERSION \
+  -n bowser \
+  -m "Andrei Zbikowski <b1naryth1ef@gmail.com>" \
+  --url "https://github.com/b1naryth1ef/bowser" \
+  --deb-upstart upstart/bowser.conf \
+  build/=/
+
+rm -r build
